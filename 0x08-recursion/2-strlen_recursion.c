ef24f1d51dae3f06c56bfc09a0962eb741f527dc
Je4stine <je4stine@gmail.com>
2022-03-30 19:54:57 +0300

    Recursions cursing me LOL

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,5 +7,15 @@
  */
 int _strlen_recursion(char *s)
 {
-	return (strlen(s));
+	if (s[0] == '\0')
+		return (0);
+
+	if (s[1] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (_strlen_recursion(&s[1]) + 1);
+	}
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,14 +6,28 @@
  * Return: square root of n
  */
 
+int sqr_con(int n, int i)
+{
+	if (n == i * i)
+		return (i);
+
+	else if (n < i * i)
+		return (-1);
+
+	return (sqr_con(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: number to square
+ * Description: find square root of n using recursion
+ * Return: square root of n or -1 if none
+ **/
+
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 		return (-1);
 
-	else if (n == (int)sq_root(n) * (int)sq_root(n))
-		return (sqrt(n));
-
-	else
-		return (-1);
+	return (sqr_con(n, 0));
 }
