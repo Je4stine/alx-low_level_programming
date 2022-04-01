2a4a6dbf77e3ec4eb017fcd26639033ee1ed8ea7
Je4stine <je4stine@gmail.com>
2022-04-01 15:13:07 +0300

    Corrections made

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,32 +8,37 @@
  * @argv: array of arguments (strings) passed to main
  * Return: 0 if successful, otherwise 1
  */
+
 int main(int argc, char *argv[])
 {
 	int i, res = 0, flag = 0;
-	
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+
+	if (argc <= 1)
+		res = 0;
 
 	else
 	{
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (isdigit(argv[i]))
+		for (i = 1; i < argc; i++)
+		{
+			if (!(isdigit(*argv[i])))
+			{
+				flag = 1;
+			}
+
 			res += atoi(&(*argv[i]));
-
-		else
-			flag = 1;
+		}
 	}
 
-	if (flag == 1)
+	if (flag == 1 && res != 0)
 	{
 		printf("Error\n");
 		return (1);
+	}
+
+	else if (flag == 1 && res == 0)
+	{
+		printf("%d\n", res);
+		return (0);
 	}
 
 	else
